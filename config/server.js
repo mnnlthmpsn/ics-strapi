d0066d8c71b742c6672f33e514ef2cceb9a3c2ac
Emmanuel Thompson <mnnlthmpsn@outlook.com>
2021-08-02 13:29:29 +0000

    ics first push strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4d52dedaf90292de9cb58deecb4480db'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f1889485-aed3-4782-a1df-e835fe20ab02'
+};
