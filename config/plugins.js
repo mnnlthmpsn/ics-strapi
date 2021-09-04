8b05b23e50426c6f5487a68179bef9c7ae7aaca4
unknown <emmanuel@appsnmobilesolutions.com>
2021-09-04 01:09:18 +0000

    report added

--- config/plugins.js
+++ config/plugins.js
@@ -5,6 +5,7 @@ module.exports = ({ env }) => ({
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_API_KEY'),
         api_secret: env('CLOUDINARY_API_SECRET'),
+        secure: true
       },
     },    
   });
